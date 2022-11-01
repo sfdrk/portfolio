1958a2be4a8f20e3b7408887c5845dd5be8de0ba
safdar <safdar@tncitgroup.com>
2022-11-01 15:15:46 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    $(".first-para").css({"transform":"translate(0)"});
+    $(".second-para").css({"transform":"translate(0)"});
+    setTimeout(function() {
+    $(".name-block").css({"opacity":"1"});
+    },400)
+});
